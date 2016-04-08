042a6550e0397d99d48054cc232831763823378f
Summer <zhangjian@chinamobo.com>
2016-04-08 10:51:58 +0800

    start

---
+++ ReactiveCocoaTest/ReactiveCocoaTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ReactiveCocoaTest
+//
+//  Created by Summer on 16/4/8.
+//  Copyright © 2016年 Summer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ReactiveCocoaTest/ReactiveCocoaTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ReactiveCocoaTest
+//
+//  Created by Summer on 16/4/8.
+//  Copyright © 2016年 Summer. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
